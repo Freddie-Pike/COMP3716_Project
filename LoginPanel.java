fe123c00ad8cd718f659ff63ae7a9cd5b74498ff
Drolfb <thedecoymove@hotmail.com>
2016-11-08 20:50:57 -0330

    Add files via upload

---
+++ LoginPanel.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class LoginPanel extends JPanel
+{
+
+    private JLabel Header;
+    private JLabel Q;
+    private JTextField UserName;
+    private String[] languages = {
+            "Player", "Tournament Organiser", "Referee", "Tournament Manager",
+            "Coach"
+    };
+    private JComboBox comboBox = new JComboBox();
+    private int count = 0;
+    private JButton Submit;
+
+    public LoginPanel()
+    {
+        setBackground(Color.blue);
+        for(int i = 0; i < languages.length; i++)
+            comboBox.addItem(languages[count++]);
+        Header = new JLabel("Welcome to the Login Page");
+        Font f = new Font("Comic Sans MS", Font.BOLD, 16);
+        JPanel DatLogin = new JPanel();
+        DatLogin.setPreferredSize(new Dimension(280,80));
+        Header.setFont(f);
+        Q = new JLabel("Enter your name:");
+        Submit = new JButton("Submit");
+        UserName = new JTextField(15);
+        DatLogin.add(Q);
+        DatLogin.add(UserName);
+        DatLogin.add(comboBox);
+        DatLogin.add(Submit);
+        add(Header);
+        add(DatLogin);
+    }
+}
---
+++ LoginRunner.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+public class LoginRunner
+{
+    /**
+     Creates the main frame of the program.
+     */
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("Login");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 600);
+        LoginPanel panel = new LoginPanel();
+        frame.add(panel);
+        frame.setVisible(true);
+
+    }
+}
