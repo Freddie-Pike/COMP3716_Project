d86d8bfcf31b08dd803d846e4004ae3e9ca9a584
Fredmental <supervemon@hotmail.com>
2016-11-24 14:16:53 -0330

    Update DatabaseTester.java
    An Tester class that tests out updating skill levels.

--- src/tester/DatabaseTester.java
+++ src/tester/DatabaseTester.java
@@ -1,23 +1,75 @@
-package tester;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.UUID;
 
-import main.io.TextDatabaseTools;
 
 /**
- * A class that tests out the maining system information part of the architecture. It adds a row to a textfile 
+ * A class that tests out the maintaining system information part of the architecture. It adds a row to a textfile 
  * database and the prints out the contents of the database.
  * @author Freddie Pike
  */
 
+
 public class DatabaseTester {
     public static void main(String [] args) {
-        TextDatabaseTools tournamentDatabase = new TextDatabaseTools("Tournament_Information.txt");
+    	Scanner in = new Scanner(System.in);
         
+        // This block of code tests out calculating skill level.
+        
+    	// System.out.println("Do you want to create a tournament: Enter Y or N");
+    	ArrayList<String> tempArray = new ArrayList<String>();
+    	String team1[][] = new String[50][50];
+    	
+    	TextDatabaseTools teamDB = new TextDatabaseTools("Pitbulls");
+    
+    	tempArray = teamDB.grabColumnDataAsArray(1);
+    	
+    	int tempSkill = 0;
+    	int totalskill = 0;
+    	for (int i = 0; i < tempArray.size(); i++) {
+    		tempSkill = Integer.parseInt(tempArray.get(i));
+    		System.out.print(tempArray.get(i) + " ");
+    		totalskill += tempSkill;
+    	}
+    	System.out.println("\n" + totalskill);
+    	
+    	TextDatabaseTools game1 = new TextDatabaseTools("Game_123");
+    	ArrayList<String> gameColumn1 = new ArrayList<String>();
+    	
+    	gameColumn1 = game1.grabColumnDataAsArray(1);
+    	
+    	for (int i = 0; i < gameColumn1.size(); i++) {
+    		System.out.print(tempArray.get(i) + " ");
+    	}
+    	
+    	
+    	
+    	
+    	/**
+    	String uuid = UUID.randomUUID().toString();
+    	System.out.println(uuid);
+    	TextDatabaseTools tournamentDatabase = new TextDatabaseTools("Game_123");
+    	
+    	String testArray[][] = new String[10][10];
+    	testArray = tournamentDatabase.returnTextFileAsArray();
+    	
+    	testArray[1][0] = "Pitbulls";
+    	
+    	
+    	
+    	tournamentDatabase.updateTextFileUsingArray(testArray);
+    	
+    	tournamentDatabase.printContentsByColumn(1);
+    	*/
+    	
+    	
+            
+
+        // String gameName = "Game_BasicInfo_" + uuid;
+    	// TextDatabaseTools tournamentDatabase = new TextDatabaseTools(gameName); 
+        // tournamentDatabase.addRow(gameName + "/Fred/2/8/6/10/Single Elimination/2016 11 20/Madison Square Garden/2016-11-28");
+        // tournamentDatabase.addRow(gameName + "/Steve/2/10/6/12/Single Elimination-2016 8 7/MileOne/2016-10-2");
         // tournamentDatabase.printContents();
-        // TextDatabaseManager fileManager = new TextDatabaseManager();
-        // fileManager.deleteFile("Game_123456.txt");
-       
-        tournamentDatabase.removeRowByStartingColumnValue("1");
-        
-        //LocalDate today = LocalDate.now();
+    	
     }
 }
