9be1d793377ad5cf275dd466b4edc4372e6bd7fe
Fredmental <supervemon@hotmail.com>
2016-11-24 14:14:25 -0330

    Update TextDatabaseTools.java
    An unfinished update to TextDatabaseTools. In class as I'm doing this so I'll finish it up later.

--- src/main/io/TextDatabaseTools.java
+++ src/main/io/TextDatabaseTools.java
@@ -1,38 +1,38 @@
-package main.io;
-
 import java.io.*;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.EmptyStackException;
 import java.util.List;
 
-public class TextDatabaseTools implements IDatabaseTools {
+public class TextDatabaseTools implements DatabaseTools {
 	private String fileName;
+	private String[][] arrayData = new String[9001][9001]; // IT"S OVER 9000!!!!!!!!!!!
 	
 	/** 
-	 * A constructor for TextDatabaseTools. If a file doesn't exist it will create one.
+	 * A constructor for TextDatabaseTools. IMPORTANT:If a file doesn't exist it will create one.
 	 */
 	public TextDatabaseTools(String fileName) {
+		fileName += ".txt";
 		this.fileName = fileName;
 		try {
 		    File textFile = new File(fileName);
 
 		    if ( textFile.exists() && !textFile.isDirectory() ) {
-		        System.out.println(fileName + " already Exists");
-		        
+		        System.out.println(fileName + " already Exists"); // If a text file already exists then use then
 		    }
 		    else {
 		    	System.out.println(fileName + " is created");
 		    	PrintWriter writer = new PrintWriter(fileName, "UTF-8");
 		    	writer.close();
 		    }
-		} catch (Exception e) {
-		   // do something
+		} catch (Exception exception) {
+		   System.out.println("There was an error: ");
+		   exception.printStackTrace();
 		}
 	}
 		
 	/**
-	 * Print out entire contents of the text file.
+	 * Print out the entire contents of the text file.
 	 */
 	public void printContents() {
 		String line = null;
@@ -49,10 +49,11 @@ public class TextDatabaseTools implements IDatabaseTools {
 	        // Read through the text document. 
 	        while((line = bufferedReader.readLine()) != null) 
 	        {
-	        	splitLine = line.split(" ");
+	        	splitLine = line.split("/");
 	        	System.out.print("Row = " + rowCount + " ");
 	        	for (int i = 0; i < splitLine.length; i++) {
-	        		System.out.print("Column " + i + " value = " + splitLine[i] + " ");
+	        		System.out.print(splitLine[i] + " ");
+	        		// Another way to print: System.out.print("Column " + i + " value = " + splitLine[i] + " ");
 	        	}
 	        	System.out.println();
 	        	rowCount++;
@@ -60,19 +61,20 @@ public class TextDatabaseTools implements IDatabaseTools {
 	        // Always close files.
 	        bufferedReader.close();
 		}
-		catch(FileNotFoundException ex) {
+		catch(FileNotFoundException exception) {
             System.out.println("Unable to open file '" + fileName + "'");
+            exception.printStackTrace();
         }
-        catch(IOException ex) {
+        catch(IOException exception) {
             System.out.println("Error reading file '" + fileName + "'");
-            // Or we could just do this: 
-            // ex.printStackTrace();
+            exception.printStackTrace();
         }
 	}
 	
+	
 	/**
 	 * Print out the contents of a column of information from the text file.
-	 * @param column
+	 * @param column - The column to print out
 	 */
 	public void printContentsByColumn(int column) {
 		String line = null;
@@ -87,26 +89,26 @@ public class TextDatabaseTools implements IDatabaseTools {
 	        // Read through the text document. 
 	        while((line = bufferedReader.readLine()) != null) 
 	        {
-	        	splitLine = line.split(" ");
+	        	splitLine = line.split("/");
         		System.out.println(splitLine[column]);
 	        	
 	        }
 	        // Always close files.
 	        bufferedReader.close();
 		}
-		catch(FileNotFoundException ex) {
+		catch(FileNotFoundException exception) {
             System.out.println("Unable to open file '" + fileName + "'");
+            exception.printStackTrace();
         }
-        catch(IOException ex) {
+        catch(IOException exception) {
             System.out.println("Error reading file '" + fileName + "'");
-            // Or we could just do this: 
-            // ex.printStackTrace();
+            exception.printStackTrace();
         }
 	}
 
 	/**
-	 * Add line to text database.
-	 * @param line - line to be added at end of textfile
+	 * Add a line to the text file.
+	 * @param line - line to be added at end of text file
 	 */
 	public void addRow(String line) {
 		try {
@@ -118,17 +120,17 @@ public class TextDatabaseTools implements IDatabaseTools {
 		}
 		catch(FileNotFoundException ex) {
             System.out.println("Unable to open file '" + fileName + "'");
+            ex.printStackTrace();
         }
         catch(IOException ex) {
             System.out.println("Error reading file '" + fileName + "'");
-            // Or we could just do this: 
-            // ex.printStackTrace();
+            ex.printStackTrace();
         }
 	}
 	
 	/**
 	 * Remove an entire line from text database.
-	 * @param line - The line to be removed from the textfile
+	 * @param line - The line to be removed from the text file
 	 */
 	public void removeRow(String deleteLine) {
 		/**
@@ -141,7 +143,6 @@ public class TextDatabaseTools implements IDatabaseTools {
 			File tempFile = new File("tempTable.txt");
 			
 			BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-			
 			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(tempFile));
 			
 			String line; 
@@ -158,11 +159,7 @@ public class TextDatabaseTools implements IDatabaseTools {
         		bufferedWriter.newLine();
 	        }
 			
-			// TextDatabaseManager deleteOriginalFile = new TextDatabaseManager();
-			
-			
 			boolean isFileRenamed = tempFile.renameTo(textFile);
-			// deleteOriginalFile.deleteFile(fileName);
 			
 			if (isFileRenamed) {
 				System.out.println("File Was Renamed");
@@ -173,19 +170,19 @@ public class TextDatabaseTools implements IDatabaseTools {
 			bufferedWriter.close(); 
 			bufferedReader.close(); 
 		}
-		catch(FileNotFoundException ex) {
+		catch(FileNotFoundException exception) {
             System.out.println("Unable to open file '" + fileName + "'");
+            exception.printStackTrace();
         }
-        catch(IOException ex) {
+        catch(IOException exception) {
             System.out.println("Error reading file '" + fileName + "'");
-            // Or we could just do this: 
-            // ex.printStackTrace();
+            exception.printStackTrace();
         }
 	}
 	
 	/**
 	 * Remove line from text database.
-	 * @param line - The line to be removed from the textfile
+	 * @param line - The line to be removed from the text file
 	 */
 	public void removeRowByStartingColumnValue(String deleteLine) {
 		/**
@@ -193,16 +190,14 @@ public class TextDatabaseTools implements IDatabaseTools {
 		 * executable architecture. 
 		 */
 		try {
-			// Name of tempfile
+			// Name of temptable.txt 
 			File textFile = new File(fileName);
 			File tempFile = new File("tempTable.txt");
 			
 			BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-			
 			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(tempFile));
 			
 			String line; 
-			
 			boolean lineNotDeleted = true;
 			
 			while((line = bufferedReader.readLine()) != null) 
@@ -215,11 +210,136 @@ public class TextDatabaseTools implements IDatabaseTools {
         		bufferedWriter.newLine();
 	        }
 			
-			// TextDatabaseManager deleteOriginalFile = new TextDatabaseManager();
+			boolean isFileRenamed = tempFile.renameTo(textFile);
 			
+			if (isFileRenamed) {
+				System.out.println("File Was Renamed");
+			} else {
+				System.out.println("File Wasn't Renamed");
+			}
+			
+			bufferedWriter.close(); 
+			bufferedReader.close(); 
+		}
+		catch(FileNotFoundException exception) {
+            System.out.println("Unable to open file '" + fileName + "'");
+            exception.printStackTrace();
+        }
+        catch(IOException exception) {
+            System.out.println("Error reading file '" + fileName + "'");
+            exception.printStackTrace();
+        }
+	}
+	
+	/**
+	 * Remove an entire line from text database.
+	 * @param line - The line to be removed from the textfile
+	 */
+	public void removeRowByColumn(String deleteLine, int column) {
+		try {
+			// Name of tempfile
+			File textFile = new File(fileName);
+			File tempFile = new File("tempTable.txt");
+			
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+			
+			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(tempFile));
+			
+			String line; 
+			String splitLine[];
+			
+			boolean lineNotDeleted = true;
+			
+			while((line = bufferedReader.readLine()) != null) 
+	        {
+				splitLine = line.split("/");
+        		if (splitLine[column].equals(deleteLine) && lineNotDeleted ) {
+        			lineNotDeleted = false;
+        			continue; 
+        		}
+        		bufferedWriter.write(line);
+        		bufferedWriter.newLine();
+	        }
 			
 			boolean isFileRenamed = tempFile.renameTo(textFile);
-			// deleteOriginalFile.deleteFile(fileName);
+			
+			if (isFileRenamed) {
+				System.out.println("File Was Renamed");
+			} else {
+				System.out.println("File Wasn't Renamed");
+			}
+			
+			bufferedWriter.close(); 
+			bufferedReader.close(); 
+		}
+		catch(FileNotFoundException exception) {
+            System.out.println("Unable to open file '" + fileName + "'");
+            exception.printStackTrace();
+        }
+        catch(IOException exception) {
+            System.out.println("Error reading file '" + fileName + "'");
+            exception.printStackTrace();
+        }
+	}
+	
+	// Methods - Get all data as array
+	public String[][] returnTextFileAsArray() {
+		String line = null;
+		String[] splitLine = null;
+		try {
+			// FileReader reads text files in the default encoding.
+            FileReader fileReader = new FileReader(fileName);
+            
+			// Always wrap FileReader in BufferedReader.
+	        BufferedReader bufferedReader = new BufferedReader(fileReader);
+	
+	        int rowCount = 0;
+	        
+	        // Read through the text document. 
+	        while((line = bufferedReader.readLine()) != null) 
+	        {
+	        	splitLine = line.split("/");
+	        	// System.out.print("Row = " + rowCount + " ");
+	        	for (int i = 0; i < splitLine.length; i++) {
+	        		arrayData[rowCount][i] = splitLine[i];
+	        		// System.out.println(arrayData[rowCount][i]);// Another way to print: System.out.print("Column " + i + " value = " + splitLine[i] + " ");
+	        	}
+	        	rowCount++;
+	        }
+	        // Always close files.
+	        bufferedReader.close();
+	        return arrayData;
+		}
+		catch(FileNotFoundException exception) {
+            System.out.println("Unable to open file '" + fileName + "'");
+            exception.printStackTrace();
+            return arrayData;
+        }
+        catch(IOException exception) {
+            System.out.println("Error reading file '" + fileName + "'");
+            exception.printStackTrace();
+            return arrayData;
+        }
+	}
+	
+	public void updateTextFileUsingArray(String[][] updateArray) {
+		try {
+			// 
+			File textFile = new File(fileName);
+			File tempFile = new File("tempTable.txt");
+			
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(tempFile));
+			
+			for (int i = 0; i < updateArray.length; i++) {
+	    		for (int j = 0; j < updateArray.length; j++) {
+	    			if (updateArray[i][j] == null) break;
+	    			bufferedWriter.write(updateArray[i][j] + "/");
+	    		}
+	    		if (updateArray[i][0] == null) break;
+	    		bufferedWriter.newLine();
+	    	}		
+			boolean isFileRenamed = tempFile.renameTo(textFile);
 			
 			if (isFileRenamed) {
 				System.out.println("File Was Renamed");
@@ -232,23 +352,158 @@ public class TextDatabaseTools implements IDatabaseTools {
 		}
 		catch(FileNotFoundException ex) {
             System.out.println("Unable to open file '" + fileName + "'");
+            ex.printStackTrace();
         }
         catch(IOException ex) {
             System.out.println("Error reading file '" + fileName + "'");
-            // Or we could just do this: 
-            // ex.printStackTrace();
+            ex.printStackTrace();
         }
-	}
-	
-	public ArrayList<String> getRow(int row) {
-		ArrayList<String> tempList = new ArrayList();
-		return tempList;
-	}
-	
-	public void sortByColumn(int row) {
 		
 	}
 	
+	public ArrayList<String> grabRowDataAsArray(int row) {
+		String line = null;
+		String[] splitLine = null;
+		ArrayList<String> tempList = new ArrayList();
+		try {
+			// FileReader reads text files in the default encoding.
+            FileReader fileReader = new FileReader(fileName);
+            
+			// Always wrap FileReader in BufferedReader.
+	        BufferedReader bufferedReader = new BufferedReader(fileReader);
+	
+	        int rowTemp = 0;
+	        
+	        // Read through the text document. 
+	        while((line = bufferedReader.readLine()) != null) 
+	        {
+	        	if (rowTemp == row) {
+	        		splitLine = line.split("/");
+	        		for (int i = 0; i < splitLine.length; i++) {
+	        			tempList.add(splitLine[i]);
+	        		}
+	        		break;
+	        	}
+	        	rowTemp++;
+	        }
+	        // Always close files.
+	        bufferedReader.close();
+	        return tempList;
+		}
+		catch(FileNotFoundException exception) {
+            System.out.println("Unable to open file '" + fileName + "'");
+            exception.printStackTrace();
+            return tempList;
+        }
+        catch(IOException exception) {
+            System.out.println("Error reading file '" + fileName + "'");
+            exception.printStackTrace();
+            return tempList;
+        }
+	}
+	
+	public ArrayList<String> grabColumnDataAsArray(int column) {
+		String line = null;
+		String[] splitLine = null;
+		ArrayList<String> tempList = new ArrayList();
+		try {
+			// FileReader reads text files in the default encoding.
+            FileReader fileReader = new FileReader(fileName);
+            
+			// Always wrap FileReader in BufferedReader.
+	        BufferedReader bufferedReader = new BufferedReader(fileReader);
+	
+	        int rowTemp = 0;
+	        
+	        // Read through the text document. 
+	        while((line = bufferedReader.readLine()) != null) 
+	        {
+	        	splitLine = line.split("/");
+	        	tempList.add(splitLine[column]);
+	        	System.out.println("Column " + column + " " + splitLine[column]);
+	        }
+	        // Always close files.
+	        bufferedReader.close();
+	        return tempList;
+		}
+		catch(FileNotFoundException exception) {
+            System.out.println("Unable to open file '" + fileName + "'");
+            exception.printStackTrace();
+            return tempList;
+        }
+        catch(IOException exception) {
+            System.out.println("Error reading file '" + fileName + "'");
+            exception.printStackTrace();
+            return tempList;
+        }
+	}
+	
+	/**
+	 * This method updates a column of a textfile using an ArrayList. ***NOT FINISHED.
+	 * @param updateArray - The array that updates the column
+	 * @param column - update this column.
+	 */
+	public void updateColumnUsingArrayList(ArrayList<String> updateArray, int column) {
+		try {
+			// Name of tempfile
+			File textFile = new File(fileName);
+			File tempFile = new File("tempTable.txt");
+			
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+			
+			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(tempFile));
+			
+			String line; 
+			String splitLine[];
+			
+			while((line = bufferedReader.readLine()) != null) 
+	        {
+				splitLine = line.split("/");
+        		bufferedWriter.write(line);
+        		bufferedWriter.newLine();
+	        }
+			
+			boolean isFileRenamed = tempFile.renameTo(textFile);
+			
+			if (isFileRenamed) {
+				System.out.println("File Was Renamed");
+			} else {
+				System.out.println("File Wasn't Renamed");
+			}
+			
+			bufferedWriter.close(); 
+			bufferedReader.close(); 
+		}
+		catch(FileNotFoundException exception) {
+            		System.out.println("Unable to open file '" + fileName + "'");
+            		exception.printStackTrace();
+        	}
+        	catch(IOException exception) {
+            		System.out.println("Error reading file '" + fileName + "'");
+            		exception.printStackTrace();
+        	}
+	}
+	
+	
+	
+	/**
+	 * A method that sorts the text file. TO BE IMPLEMENETED IN 2ND ITERATION. 
+	 */
+	public void sort() {
+		
+	}
+	
+	/**
+	 * A method that sorts the text file by a specified column. TO BE IMPLEMENETED IN 2ND ITERATION. 
+	 * @param - column - an int value that specifies what column the text file is going to be sorted by.
+	 */
+	public void sortByColumn(int column) {
+		
+	}
+	
+	/**
+	 * Print out all the class name and the name of the textfile.
+	 */
 	@Override
 	public String toString() {
 		return "TextDatabaseManager [fileName=" + fileName + "]";
